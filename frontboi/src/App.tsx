11d80ee64422e09236dfa05bd212610ad736394b
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-05-23 20:49:16 +0000

    Sprucing frontboi

--- frontboi/src/App.tsx
+++ frontboi/src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CodeTxt } from './components/CodeTxt'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        👋 I'm <CodeTxt>binboi</CodeTxt> and I can remind you of your next bin collection day.
+      </p>
     </div>
   );
 }
---
+++ frontboi/src/components/CodeTxt.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const CodeTxt = styled.p`
+    color: red;
+`
