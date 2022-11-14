df6748ce9e272c8ba7d49f721f1a2005e4fe621e
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-11-14 05:28:54 +0000

    Changing binboi message style

--- frontboi/src/App.tsx
+++ frontboi/src/App.tsx
@@ -6,8 +6,14 @@ import { FormHero } from './components/FormHero'
 function App() {
   return (
     <div className="App">
-      <p>
-        👋 I'm <CodeTxt>binboi</CodeTxt> and I can generate a bin collection calendar for you.
+      <p style={{width: "40em", margin: "0.2em"}}>
+        👋 I'm <CodeTxt>binboi</CodeTxt> and I can generate a full year's set of bin collection reminders for your calendar. 
+      </p>
+      <p style={{width: "40em", margin: "0.2em", fontWeight: "600"}}>
+        Enter your postcode to get started.
+      </p>
+      <p style={{fontStyle: "italic", fontSize: "calc(4px + 2vmin)", margin: "1em"}}>
+        (I only working for Reading council at the moment.)
       </p>
       <FormHero/>
     </div>
