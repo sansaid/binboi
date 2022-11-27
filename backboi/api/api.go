6277e276f710139e2c113c70d913b8416a9ab600
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-11-27 06:04:33 +0000

    Creating a calendar feed in backboi

--- backboi/api/api.go
+++ backboi/api/api.go
@@ -17,6 +17,7 @@ const (
 	StatusInternalServerError = iota
 	StatusSuccess
 	StatusNotImplemented
+	StatusUnrecognisedUprn
 )
 
 type Proxy struct{}
@@ -38,6 +39,8 @@ func statusCode(s StatusCode, customMessage string) *Status {
 		return NewStatus(s, fmt.Sprintf("Something bad happened, but we're not sure why. Error: %s", customMessage))
 	case StatusSuccess:
 		return NewStatus(s, "All good here homie")
+	case StatusUnrecognisedUprn:
+		return NewStatus(s, "UPRN %s is not recognised - your address is not within the Reading jurisdiction")
 	case StatusNotImplemented:
 		return NewStatus(s, fmt.Sprintf("Sorry bruh, I'm not a real boy yet. Failed at method: %s", customMessage))
 	}
@@ -45,13 +48,11 @@ func statusCode(s StatusCode, customMessage string) *Status {
 	return NewStatus(s, fmt.Sprintf("I don't know what to tell you kiddo, but here's a code you can use to figure it out: %s", customMessage))
 }
 
-func proxyEndpoint(ctx echo.Context, endpoint string, marshalObject any) error {
+func get(endpoint string, marshalObject any) error {
 	resp, err := http.Get(endpoint)
 
 	if err != nil {
-		errorMessage := fmt.Sprintf("Error getting response from %s: %s", endpoint, err.Error())
-
-		return ctx.JSON(http.StatusInternalServerError, statusCode(StatusInternalServerError, errorMessage))
+		return fmt.Errorf("[binboi] Error getting response from %s: %s", endpoint, err.Error())
 	}
 
 	// Limit request bodies to 5MB
@@ -59,19 +60,71 @@ func proxyEndpoint(ctx echo.Context, endpoint string, marshalObject any) error {
 	body, err := ioutil.ReadAll(reader)
 
 	if err != nil {
+		return fmt.Errorf("[binboi] Error parsing response from %s: %s", endpoint, err.Error())
+	}
+
+	err = json.Unmarshal(body, &marshalObject)
+
+	if err != nil {
+		return fmt.Errorf("[binboi] Error parsing response from %s: %s", endpoint, err.Error())
+	}
+
+	return nil
+}
+
+func proxyEndpoint(ctx echo.Context, endpoint string, marshalObject any) error {
+	err := get(endpoint, marshalObject)
+
+	if err != nil {
 		return ctx.JSON(http.StatusInternalServerError, statusCode(StatusInternalServerError, err.Error()))
 	}
 
-	json.Unmarshal(body, &marshalObject)
-
 	return ctx.JSON(http.StatusOK, marshalObject)
+}
+
+func (p *Proxy) GetCalendarFeed(ctx echo.Context, uprn string, params GetCalendarFeedParams) error {
+	collectionsEndpoint := fmt.Sprintf("https://api.reading.gov.uk/api/collections/%s", uprn)
+	var collections Collections
+
+	queryParamsList := []string{}
+
+	if params.FromDate != nil {
+		queryParamsList = append(queryParamsList, "from_date="+*params.FromDate)
+	}
+
+	if params.ToDate != nil {
+		queryParamsList = append(queryParamsList, "to_date="+*params.ToDate)
+	}
+
+	if len(queryParamsList) > 0 {
+		queryParams := utils.BuildQueryParams(queryParamsList)
+		collectionsEndpoint = collectionsEndpoint + "?" + queryParams
+	}
+
+	err := get(collectionsEndpoint, &collections)
+
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, statusCode(StatusInternalServerError, err.Error()))
+	}
+
+	if len(*collections.Collections) == 0 {
+		return ctx.JSON(http.StatusBadRequest, statusCode(StatusUnrecognisedUprn, uprn))
+	}
+
+	cal, err := CollectionsToCal(collections.Collections)
+
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, statusCode(StatusInternalServerError, err.Error()))
+	}
+
+	return ctx.Blob(http.StatusOK, "text/calendar", []byte(cal.Serialize()))
 }
 
 func (p *Proxy) GetAddresses(ctx echo.Context, postcode string) error {
 	endpoint := fmt.Sprintf("https://api.reading.gov.uk//rbc/getaddresses/%s", postcode)
 	var addresses Addresses
 
-	return proxyEndpoint(ctx, endpoint, addresses)
+	return proxyEndpoint(ctx, endpoint, &addresses)
 }
 
 func (p *Proxy) GetCollectionDates(ctx echo.Context, uprn string, params GetCollectionDatesParams) error {
@@ -93,7 +146,7 @@ func (p *Proxy) GetCollectionDates(ctx echo.Context, uprn string, params GetColl
 		endpoint = endpoint + "?" + queryParams
 	}
 
-	return proxyEndpoint(ctx, endpoint, collections)
+	return proxyEndpoint(ctx, endpoint, &collections)
 }
 
 func (p *Proxy) Health(ctx echo.Context) error {
